6b09b49eb9ef814fe35d150a2ea17e8372012a09
Arthur Bogacki <abverissimo@inf.ufsm.br>
2023-11-21 16:17:31 -0300

    Finalizando trab5

--- abb.h
+++ abb.h
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 struct lista{
     char nome[25];
     int matricula;
@@ -20,3 +21,8 @@ typedef struct arv Arv;
 
 Arv* abb_insere(Arv* a, int v);
 void abb_imprime(Arv* a);
+bool codIndisponivel(Arv* a, int cod);
+Arv* achaCod(Arv* arvore, int codCurso);
+Lista* insereListaAluno(Arv* arvore, int codCurso, char nomeAluno[], int matricula, int anoIngresso);
+Lista* excluiAlunoLista(Arv* arvore, int mat);
+Arv* abb_retira (Arv* r, int v);
